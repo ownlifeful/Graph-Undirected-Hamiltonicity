cfa31c01d80693c0db500f29f8f4f09f7f70f601
Ashwin Dixit <ashwin@ownlifeful.com>
2016-11-06 16:43:03 -0500

    Heed Perl::Critic and loop rather than modifying $_ in a list function.

--- lib/Graph/Undirected/Hamiltonicity/Transforms.pm
+++ lib/Graph/Undirected/Hamiltonicity/Transforms.pm
@@ -334,7 +334,13 @@ sub string_to_graph {
 
     foreach my $chunk ( split ( /\,/, $string ) ) {
         if ( $chunk =~ /=/ ) {
-            my @endpoints = map { s/^0+([1-9])/$1/; $_ } ( split ( /=/, $chunk ) );
+
+            my @endpoints;
+            foreach my $endpoint ( split ( /=/, $chunk ) ) {
+                my $vertex = ( $endpoint =~ s/\b0+([1-9])/$1/gr );
+                push @endpoints, $vertex;
+            }
+
             next if $endpoints[0] == $endpoints[1];
             push @edges, \@endpoints;
             $vertices{ $endpoints[0] } = 1;
