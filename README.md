f8038dab7806dcfc6016ae71123323fd538dc5b6
Ashwin Dixit <ashwin@cpan.org>
2022-10-01 04:39:27 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+
+# NAME
+
+Graph::Undirected::Hamiltonicity - It's new $module
+
+# SYNOPSIS
+
+    use Graph::Undirected::Hamiltonicity;
+
+# DESCRIPTION
+
+Graph::Undirected::Hamiltonicity is ...
+
+# LICENSE
+
+Copyright (C) Ashwin Dixit.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ashwin Dixit <ashwin@cpan.org>
---
+++ lib/Graph/Undirected/Hamiltonicity.pm
@@ -0,0 +1,39 @@
+package Graph::Undirected::Hamiltonicity;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Graph::Undirected::Hamiltonicity - It's new $module
+
+=head1 SYNOPSIS
+
+    use Graph::Undirected::Hamiltonicity;
+
+=head1 DESCRIPTION
+
+Graph::Undirected::Hamiltonicity is ...
+
+=head1 LICENSE
+
+Copyright (C) Ashwin Dixit.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ashwin Dixit E<lt>ashwin@cpan.orgE<gt>
+
+=cut
+
